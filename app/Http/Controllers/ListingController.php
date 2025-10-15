4fd947728b9eda722ccb690891f92a5b8cb5aea5
dania99-b <dania1999ta@gmail.com>
2025-10-16 01:54:42 +0300

    Initial commit for Laravel project

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Listing;
+
+class ListingController extends Controller
+{
+    public function index()
+    {
+        $listings = Listing::all();
+
+        // أضف حقل source لكل عنصر
+        $listings = $listings->map(function ($listing) {
+            return [
+                'id' => $listing->id,
+                'title' => $listing->title,
+                'price' => $listing->price,
+                'views' => $listing->views,
+                'source' => 'laravel'
+            ];
+        });
+
+        return response()->json($listings);
+    }
+}
